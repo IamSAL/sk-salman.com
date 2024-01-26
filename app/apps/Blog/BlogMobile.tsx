e03fc1ac22c63067286fe4a2b235156f2edfb87f
Sheikh Salman <sheikhsalman2013@gmail.com>
2024-01-26 23:28:06 +0600

    blog page template

--- app/apps/Blog/BlogMobile.tsx
+++ app/apps/Blog/BlogMobile.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 
 import { useAppContext } from 'app/components/app-window/appContext';
 
-
+//TODO: Embed hasnode react tw template
 const BlogMobile = () => {
     const isPreview = document.location.href.includes('examplePreview');
     const { onTerminate } = useAppContext()
